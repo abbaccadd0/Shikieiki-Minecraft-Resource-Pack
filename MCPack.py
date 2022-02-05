b0fb3e764e54d2e3acecb50306ccacfdffc8b40f
abbaccadd0 <abbaccadd0@qq.com>
2022-02-05 11:46:35 +0800

    update resource pack

--- MCPack.py
+++ MCPack.py
@@ -4,11 +4,12 @@ import wget
 import zipfile
 
 print("正在下载资源包")
-url = 'https://ghproxy.com/https://github.com/CFPAOrg/Minecraft-Mod-Language-Package/releases/latest/download/Minecraft-Mod-Language-Package-1.16.zip'
+url = 'http://downloader1.meitangdehulu.com:22943/Minecraft-Mod-Language-Modpack-1-16.zip'
+# url = 'https://ghproxy.com/https://github.com/CFPAOrg/Minecraft-Mod-Language-Package/releases/latest/download/Minecraft-Mod-Language-Package-1.16.zip'
 r = wget.download(url)
 
 print("\n正在解压资源包")
-zFile = zipfile.ZipFile(".\\Minecraft-Mod-Language-Package-1.16.zip", "r")
+zFile = zipfile.ZipFile(".\\Minecraft-Mod-Language-Modpack-1-16.zip", "r")
 for fileM in zFile.namelist():
     zFile.extract(fileM, ".\\Shikieiki-Minecraft-Resource-Pack")
 zFile.close()
@@ -22,7 +23,7 @@ os.remove(".\\Shikieiki-Minecraft-Resource-Pack\\license")
 os.remove(".\\Shikieiki-Minecraft-Resource-Pack\\readme.md")
 os.remove(".\\Shikieiki-Minecraft-Resource-Pack\\pack.mcmeta")
 os.remove(".\\Shikieiki-Minecraft-Resource-Pack\\pack.png")
-os.remove("Minecraft-Mod-Language-Package-1.16.zip")
+os.remove("Minecraft-Mod-Language-Modpack-1-16.zip")
 
 print("正在添加版本信息")
 shutil.copy('pack.mcmeta', '.\\Shikieiki-Minecraft-Resource-Pack')
